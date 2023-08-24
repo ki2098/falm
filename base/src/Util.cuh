f8f38e7fb9935a7cf58e15fe2f772faa9b4e10e9
ki2098 <1336670149@qq.com>
2023-08-24 22:28:16 +0900

    big adjustment on indexing

---
+++ base/src/Util.cuh
@@ -0,0 +1,38 @@
+#ifndef _UTIL_H_
+#define _UTIL_H_ 1
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+namespace FALMLoc {
+static const unsigned int NONE   = 0;
+static const unsigned int HOST   = 1;
+static const unsigned int DEVICE = 2;
+static const unsigned int BOTH   = HOST | DEVICE;
+}
+
+namespace FALMUtil {
+
+__host__ __device__ static inline void d123(unsigned int idx, unsigned int &i, unsigned int &j, unsigned int &k, dim3 &size) {
+    unsigned int slice = size.y * size.z;
+    i   = idx / slice;
+    idx = idx % slice;
+    j   = idx / size.z;
+    k   = idx % size.z;
+}
+
+__host__ __device__ static inline unsigned int d321(unsigned int i, unsigned int j, unsigned int k, dim3 &size) {
+    return i * size.y * size.z + j * size.z + k;
+}
+
+__device__ static inline unsigned int get_global_idx() {
+    return blockDim.x * blockIdx.x + threadIdx.x;
+}
+
+__device__ static inline unsigned int get_global_size() {
+    return blockDim.x * gridDim.x;
+}
+
+}
+
+#endif
