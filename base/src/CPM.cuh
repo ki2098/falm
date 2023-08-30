0f4b27241a3bfa3a8fddb0d8432d4de73861d89a
ki2098 <1336670149@qq.com>
2023-08-30 10:30:37 +0900

    add colored buffer unpacking

--- base/src/CPM.cuh
+++ base/src/CPM.cuh
@@ -82,7 +82,7 @@ __global__ static void cpm_pack_buffer_colored_kernel(double *src, double *buffe
     }
 }
 
-static void cpm_pack_buffer_colored(double *src, Mapper &domain, Request &req,unsigned int color, unsigned int loc) {
+static void cpm_pack_buffer_colored(double *src, Mapper &domain, Request &req, unsigned int color, unsigned int loc) {
     Mapper &range = req.map;
     unsigned int ref_color = (domain.offset.x + domain.offset.y + domain.offset.z + range.offset.x + range.offset.y + range.offset.z) % 2;
     unsigned int color_num = range.num / 2;
@@ -109,7 +109,7 @@ static void cpm_pack_buffer_colored(double *src, Mapper &domain, Request &req,un
     req.buflen = color_num;
 }
 
-__global__ static void cpm_unpack_buffer_kernel (double *dst, double *buffer, Mapper domain, Mapper range) {
+__global__ static void cpm_unpack_buffer_kernel(double *dst, double *buffer, Mapper domain, Mapper range) {
     unsigned int i, j, k;
     UTIL::THREAD2IJK(i, j, k);
     dim3 &size = domain.size;
@@ -139,6 +139,46 @@ static void cpm_unpack_buffer(double *dst, Mapper &domain, Request &req, unsigne
             (range.size.z + block.z - 1) / block.z
         );
         cpm_unpack_buffer_kernel<<<grid, block>>>(dst, buffer, domain, range);
+        cudaFree(req.buffer);
+        req.buffer == nullptr;
+    }
+}
+
+__global__ static void cpm_unpack_buffer_kernel_colored (double *dst, double *buffer, Mapper domain, Mapper range, unsigned int color) {
+    unsigned int i, j, k;
+    UTIL::THREAD2IJK(i, j, k);
+    dim3 &size = domain.size;
+    dim3 &origin = domain.offset;
+    if (i < range.size.x && j < range.size.y && k < range.size.z) {
+        unsigned int buffer_idx = UTIL::IDX(i, j, k, range.size) / 2;
+        i += range.offset.x;
+        j += range.offset.y;
+        k += range.offset.z;
+        unsigned int dst_idx = UTIL::IDX(i, j, k, size);
+        i += domain.offset.x;
+        j += domain.offset.y;
+        k += domain.offset.z;
+        if ((i + j + k) % 2 == color) {
+            dst[dst_idx] = buffer[buffer_idx];
+        }
+    }
+}
+
+static void cpm_unpack_buffer_colored(double *dst, Mapper &domain, Request &req, unsigned int color, unsigned int loc) {
+    Mapper &range = req.map;
+    if (loc == LOC::DEVICE) {
+        double *buffer = (double*)req.buffer;
+        dim3 block(
+            min(range.size.x, block_size.x), 
+            min(range.size.y, block_size.y), 
+            min(range.size.z, block_size.z)
+        ); 
+        dim3 grid(
+            (range.size.x + block.x - 1) / block.x,
+            (range.size.y + block.y - 1) / block.y,
+            (range.size.z + block.z - 1) / block.z
+        );
+        cpm_unpack_buffer_kernel_colored<<<grid, block>>>(dst, buffer, domain, range, color);
         cudaFree(req.buffer);
         req.buffer == nullptr;
     }
