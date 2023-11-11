08ac72e54bd11729b8ee8c0194dafd948be4b16d
ki2098 <1336670149@qq.com>
2023-11-11 21:27:24 +0900

    disable cuda ipc by openmpi

--- app/turbine1/run.sh
+++ app/turbine1/run.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/bash
 #PJM -L "rscunit=ito-b"
-#PJM -L "rscgrp=ito-g-4"
-#PJM -L "vnode=1"
+#PJM -L "rscgrp=ito-g-16"
+#PJM -L "vnode=2"
 #PJM -L "vnode-core=36"
 #PJM -L "elapse=02:00:00"
 #PJM -j
 #PJM -X
-#PJM -o "turbine1.4.log"
+#PJM -o "turbine1.8.log"
 
 module load nvhpc/nvhpc_20.11
 
 date
 
-mpirun -np 4 --map-by ppr:2:socket -display-devel-map -display-devel-map --mca btl_smcuda_use_cuda_ipc 0 --mca plm_rsh_agent /bin/pjrsh -machinefile ${PJM_O_NODEINF} ./bin/t1
+mpirun -np 8 --map-by ppr:2:socket -display-devel-map -display-devel-map --mca plm_rsh_agent /bin/pjrsh -machinefile ${PJM_O_NODEINF} ./bin/t1
+
+#  --mca btl_smcuda_use_cuda_ipc 0
 
 tar cvzf data.tar.gz data
