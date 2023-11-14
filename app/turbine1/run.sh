0be4d45c097e446da433faffa1002b4e42d6e6f2
ki2098 <1336670149@qq.com>
2023-11-14 13:08:03 +0900

    dummy

--- app/turbine1/run.sh
+++ app/turbine1/run.sh
@@ -6,13 +6,13 @@
 #PJM -L "elapse=03:00:00"
 #PJM -j
 #PJM -X
-#PJM -o "turbine1.900x300x300.1.log"
+#PJM -o "turbine1.900x300x300.2.log"
 
 module load nvhpc/nvhpc_20.11
 
 date
 
-mpirun -np 1 --map-by ppr:2:socket -display-devel-map -display-devel-map --mca btl_smcuda_use_cuda_ipc 0 --mca plm_rsh_agent /bin/pjrsh -machinefile ${PJM_O_NODEINF} ./bin/t1 large
+mpirun -np 2 --map-by ppr:2:socket -display-devel-map -display-devel-map --mca btl_smcuda_use_cuda_ipc 0 --mca plm_rsh_agent /bin/pjrsh -machinefile ${PJM_O_NODEINF} ./bin/t1 large
 
 # --mca btl_smcuda_use_cuda_ipc 0
 # nvprof --log-file %q{OMPI_COMM_WORLD_RANK}.prof
