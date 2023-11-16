780247c4073dc48619c8ddeacacf5dc60c52fbd3
ki2098 <1336670149@qq.com>
2023-11-16 19:16:37 +0900

    change -np

--- app/turbine1/run.sh
+++ app/turbine1/run.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/bash
 #PJM -L "rscunit=ito-b"
 #PJM -L "rscgrp=ito-g-16"
-#PJM -L "vnode=2"
+#PJM -L "vnode=3"
 #PJM -L "vnode-core=36"
 #PJM -L "elapse=01:00:00"
 #PJM -j
 #PJM -X
-#PJM -o "turbine1.900x300x300.8.log"
+#PJM -o "turbine1.900x300x300.12.log"
 
 module load nvhpc/nvhpc_20.11
 
 date
 
-mpirun -np 8 --map-by ppr:2:socket -display-devel-map -display-devel-map --mca btl_smcuda_use_cuda_ipc 0 --mca plm_rsh_agent /bin/pjrsh -machinefile ${PJM_O_NODEINF} ./bin/t1 large
+mpirun -np 12 --map-by ppr:2:socket -display-devel-map -display-devel-map --mca btl_smcuda_use_cuda_ipc 0 --mca plm_rsh_agent /bin/pjrsh -machinefile ${PJM_O_NODEINF} ./bin/t1 large
 
 # --mca btl_smcuda_use_cuda_ipc 0
 # nvprof --log-file %q{OMPI_COMM_WORLD_RANK}.prof
