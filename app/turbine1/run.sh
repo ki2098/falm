f93d3c0057b0a2ef0cc53dfc04caad819611e230
ki2098 <1336670149@qq.com>
2023-11-25 14:49:36 +0900

    change -np

--- app/turbine1/run.sh
+++ app/turbine1/run.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/bash
 #PJM -L "rscunit=ito-b"
-#PJM -L "rscgrp=ito-g-16"
-#PJM -L "vnode=3"
+#PJM -L "rscgrp=ito-g-64"
+#PJM -L "vnode=6"
 #PJM -L "vnode-core=36"
 #PJM -L "elapse=01:00:00"
 #PJM -j
 #PJM -X
-#PJM -o "turbine1.weak.12.log"
+#PJM -o "turbine1.weak.24.log"
 
 module load nvhpc/nvhpc_20.11
 
 date
 
-mpirun -np 12 --map-by ppr:2:socket:PE=9 --bind-to core --report-bindings --mca plm_rsh_agent /bin/pjrsh -machinefile ${PJM_O_NODEINF} ./bin/t1 weak/12
+mpirun -np 24 --map-by ppr:2:socket:PE=9 --bind-to core --report-bindings --mca plm_rsh_agent /bin/pjrsh -machinefile ${PJM_O_NODEINF} ./bin/t1 weak/24
 
 # --mca mpi_leave_pinned 0
 # --mca btl_smcuda_use_cuda_ipc 0
