47096b3d94f99d10c80f9e55325bd10444f2a1d5
ki2098 <1336670149@qq.com>
2024-01-03 18:48:11 +0900

    change np

--- app/turbine-json/run.sh
+++ app/turbine-json/run.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/bash
 #PJM -L "rscunit=ito-b"
-#PJM -L "rscgrp=ito-g-16"
-#PJM -L "vnode=4"
+#PJM -L "rscgrp=ito-g-64"
+#PJM -L "vnode=6"
 #PJM -L "vnode-core=36"
 #PJM -L "elapse=01:00:00"
 #PJM -j
 #PJM -X
-#PJM -o "turbine.16.log"
+#PJM -o "turbine.24.log"
 
 module load nvhpc/nvhpc_20.11
 
 date
 
-mpirun -n 16 --map-by ppr:2:socket:PE=9 --bind-to core --report-bindings --mca plm_rsh_agent /bin/pjrsh -machinefile ${PJM_O_NODEINF} ./bin/main
+mpirun -n 24 --map-by ppr:2:socket:PE=9 --bind-to core --report-bindings --mca plm_rsh_agent /bin/pjrsh -machinefile ${PJM_O_NODEINF} ./bin/main
