503228a7985619a1f3328dc05653d88966c165e4
ki2098 <1336670149@qq.com>
2024-01-04 21:07:41 +0900

    add weak scaling

--- app/turbine-json/weakrun.sh
+++ app/turbine-json/weakrun.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/bash
 #PJM -L "rscunit=ito-b"
 #PJM -L "rscgrp=ito-g-16"
-#PJM -L "vnode=4"
+#PJM -L "vnode=3"
 #PJM -L "vnode-core=36"
 #PJM -L "elapse=01:00:00"
 #PJM -j
 #PJM -X
-#PJM -o "turbine.weak16.log"
+#PJM -o "turbine.weak12.log"
 
 module load nvhpc/nvhpc_20.11
 
 date
 
-cp setup.weak16.json setup.json
+cp setup.weak12.json setup.json
 
-mpirun -n 16 --map-by ppr:2:socket:PE=9 --bind-to core --report-bindings --mca plm_rsh_agent /bin/pjrsh -machinefile ${PJM_O_NODEINF} ./bin/main
+mpirun -n 12 --map-by ppr:2:socket:PE=9 --bind-to core --report-bindings --mca plm_rsh_agent /bin/pjrsh -machinefile ${PJM_O_NODEINF} ./bin/main
--- src/CPMBufMan.h
+++ src/CPMBufMan.h
@@ -1,12 +1,44 @@
-#include <vector>
-#include "CPMBuffer.h"
+#include "region.h"
+#include "devdefine.h"
 
 namespace Falm {
 
+class CpmBuffer {
+public:
+    void *dptr = nullptr;
+    void *hptr = nullptr;
+    bool active = false;
+    Region map;
+    int count = 0;
+    size_t capacity = 0;
+    size_t width = 0;
+    INT color;
+    FLAG hdc;
+};
+
 class CpmBufMan {
 public:
-    
-
+    static const int NBUFFER = 108;
+public:
+    int max_used_idx = 0;
+    CpmBuffer buffer[NBUFFER];
+    FLAG hdc;
+    int request(size_t width, const Region &map, const Region &pdm, INT color) {
+        INT refcolor = (SUM3(pdm.offset) + SUM3(map.offset)) % 2;
+        int count = map.size / 2;
+        if (map.size % 2 == 1 && refcolor == color) {
+            count ++;
+        }
+        size_t reqsize = width * count;
+        int first_vacant = -1;
+        for (int i = 0; i <= max_used_idx; i ++) {
+            if (!buffer[i].active) {
+                if (first_vacant == -1) {
+                    first_vacant = i;
+                }
+            }
+        }
+    }
 };
 
 }
