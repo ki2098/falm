924d8a55aee8a4eaab3d62274261705decf3f9b6
ki2098 <1336670149@qq.com>
2024-01-12 21:09:08 +0900

    chang -np

--- app/turbine-json/weakrun.sh
+++ app/turbine-json/weakrun.sh
@@ -12,6 +12,6 @@ module load nvhpc/nvhpc_20.11
 
 date
 
-cp setup.weak4.json setup.json
+cp setup.weak8.json setup.json
 
 mpirun -n 8 --map-by ppr:2:socket:PE=9 --bind-to core --report-bindings --mca plm_rsh_agent /bin/pjrsh -machinefile ${PJM_O_NODEINF} ./bin/main
