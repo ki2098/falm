d38c0e349b2abf3cc0767bcb0f5c168cd9cbae4e
ki2098 <1336670149@qq.com>
2024-01-13 16:19:58 +0900

    chang -np

--- app/turbine-json/weakrun.sh
+++ app/turbine-json/weakrun.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/bash
 #PJM -L "rscunit=ito-b"
 #PJM -L "rscgrp=ito-g-16"
-#PJM -L "vnode=1"
+#PJM -L "vnode=2"
 #PJM -L "vnode-core=36"
 #PJM -L "elapse=01:00:00"
 #PJM -j
 #PJM -X
-#PJM -o "turbine.bmweak4.log"
+#PJM -o "turbine.bmweak8.log"
 
 module load nvhpc/nvhpc_20.11
 
 date
 
-cp setup.weak4.json setup.json
+cp setup.weak8.json setup.json
 
-mpirun -n 4 --map-by ppr:2:socket:PE=9 --bind-to core --report-bindings --mca plm_rsh_agent /bin/pjrsh -machinefile ${PJM_O_NODEINF} ./bin/main
+mpirun -n 8 --map-by ppr:2:socket:PE=9 --bind-to core --report-bindings --mca plm_rsh_agent /bin/pjrsh -machinefile ${PJM_O_NODEINF} ./bin/main
