4a28cfcf66e200cdb44756eb0f75a1b62ce90af7
ki2098 <1336670149@qq.com>
2024-01-16 15:39:02 +0900

    partially endable pinned memory

--- app/turbine-json/bmrun.sh
+++ app/turbine-json/bmrun.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
 #PJM -L "rscunit=ito-b"
 #PJM -L "rscgrp=ito-g-16"
-#PJM -L "vnode=4"
+#PJM -L "vnode=1"
 #PJM -L "vnode-core=36"
 #PJM -L "elapse=01:00:00"
 #PJM -j
 #PJM -X
-#PJM -o "turbine.bmx16.log"
+#PJM -o "turbine.bmxx4.log"
 
 module load nvhpc/nvhpc_20.11
 
@@ -14,4 +14,4 @@ date
 
 cp setup.strong.json setup.json
 
-mpirun -n 16 --map-by ppr:2:socket:PE=9 --bind-to core --report-bindings --mca plm_rsh_agent /bin/pjrsh -machinefile ${PJM_O_NODEINF} ./bin/main
+mpirun -n 4 --map-by ppr:2:socket:PE=9 --bind-to core --report-bindings --mca plm_rsh_agent /bin/pjrsh -machinefile ${PJM_O_NODEINF} ./bin/main
--- src/MVDevCall.h
+++ src/MVDevCall.h
@@ -19,11 +19,12 @@ static void init() {
 
 static void request_reduction_buffer(INT bufsize) {
     if (reduction_buffer_size < bufsize) {
+        printf("reduction buffer enlarged from %d to %d\n", reduction_buffer_size, bufsize);
         if (reduction_buffer_size != 0) {
-            falmErrCheckMacro(falmFreePinned(reduction_buffer_host));
+            falmErrCheckMacro(falmFree(reduction_buffer_host));
             falmErrCheckMacro(falmFreeDevice(reduction_buffer_device));
         }
-        falmErrCheckMacro(falmMallocPinned((void**)&reduction_buffer_host, bufsize * sizeof(REAL)));
+        falmErrCheckMacro(falmMalloc((void**)&reduction_buffer_host, bufsize * sizeof(REAL)));
         falmErrCheckMacro(falmMallocDevice((void**)&reduction_buffer_device, bufsize * sizeof(REAL)));
         reduction_buffer_size = bufsize;
     }
@@ -31,7 +32,7 @@ static void request_reduction_buffer(INT bufsize) {
 
 static void release() {
     if (reduction_buffer_size > 0) {
-        falmErrCheckMacro(falmFreePinned(reduction_buffer_host));
+        falmErrCheckMacro(falmFree(reduction_buffer_host));
         falmErrCheckMacro(falmFreeDevice(reduction_buffer_device));
     }
     reduction_buffer_size = 0;
