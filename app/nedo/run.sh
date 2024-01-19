5949dc00328c593aa5f3773c148374803d93e728
ki2098 <1336670149@qq.com>
2024-01-19 14:02:46 +0900

    add openmp for reconstructor

--- app/nedo/run.sh
+++ app/nedo/run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 #PJM -L "rscunit=ito-b"
-#PJM -L "rscgrp=ito-g-4"
-#PJM -L "vnode=1"
+#PJM -L "rscgrp=ito-g-16"
+#PJM -L "vnode=2"
 #PJM -L "vnode-core=36"
-#PJM -L "elapse=05:00:00"
+#PJM -L "elapse=12:00:00"
 #PJM -j
 #PJM -X
 #PJM -o "nedo.log"
@@ -14,6 +14,6 @@ date
 
 . ./env
 
-mpirun -n 4 --map-by ppr:2:socket:PE=9 --bind-to core --report-bindings --mca plm_rsh_agent /bin/pjrsh -machinefile ${PJM_O_NODEINF} ./bin/main
+mpirun -n 8 --map-by ppr:2:socket:PE=9 --bind-to core --report-bindings --mca plm_rsh_agent /bin/pjrsh -machinefile ${PJM_O_NODEINF} ./bin/main
 
 ${falmdir}/bin/reconstructor data/uvwp
