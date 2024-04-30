fca8a783733687641ead672d7eacf0b61b6b5106
ki2098 <1336670149@qq.com>
2024-04-30 17:38:30 +0900

    add runscript for wisteria

---
+++ app/nedo/wisrun.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+#PJM -g gg18
+#PJM -o nedo.log
+#PJM -j
+#PJM -X
+#PJM -L rscgrp=small-a
+#PJM -L node=1
+#PJM -L elapse=03:00:00
+#PJM --mpi proc=8
+
+date
+. ./env
+
+module load nvidia
+module load nvmpi
+module list
+
+mpiexec -machinefile $PJM_O_NODEINF -n $PJM_MPI_PROC -npernode 8 --report-bindings .bin/main
+
+date
