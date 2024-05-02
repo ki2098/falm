3faf8cd7a1fe33c151fde0c380c27add09a36707
ki2098 <1336670149@qq.com>
2024-05-02 12:49:47 +0900

    add useful scripts

---
+++ script/file-clearer.py
@@ -0,0 +1,34 @@
+import json
+import sys
+import os
+
+def makefilename(prefix, step, rank):
+    return "%s_%06d_%010d"%(prefix, rank, step)
+
+prefix = sys.argv[1]
+indexFile = open(prefix+".json")
+data = json.load(indexFile)
+
+mpisize = len(data["ranks"])
+nsteps = len(data["outputSteps"])
+
+print("%d ranks and %d outputs"%(mpisize, nsteps))
+
+for snapshot in data["outputSteps"]:
+    step = snapshot["step"]
+    for rank in range(mpisize):
+        filename = makefilename(prefix, step, rank)
+        if os.path.exists(filename):
+            os.remove(filename)
+        else:
+            print("ERROR: data file %s does not exist"%(filename))
+        if "timeAvg" in snapshot:
+            filename = makefilename(prefix+"_tavg", step, rank)
+            if os.path.exists(filename):
+                os.remove(filename)
+            else:
+                print("ERROR: data file %s does not exist"%(filename))
+
+
+
+indexFile.close()
