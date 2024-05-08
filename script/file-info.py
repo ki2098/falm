81f32eb171b83624d77cba590077f6cc2b92c8fd
ki2098 <1336670149@qq.com>
2024-05-08 12:32:26 +0900

    add data file info output

--- script/file-info.py
+++ script/file-info.py
@@ -1,18 +1,20 @@
 import numpy as np
 import sys
 
-path = sys.argv[1]
+file=open(sys.argv[1], 'rb')
 
-imax = np.fromfile(path, dtype='uint64', count=1, offset=0)
-jmax = np.fromfile(path, dtype='uint64', count=1, offset=8)
-kmax = np.fromfile(path, dtype='uint64', count=1, offset=16)
-nvar = np.fromfile(path, dtype='uint64', count=1, offset=24)
-gc   = np.fromfile(path, dtype='uint64', count=1, offset=32)
-step = np.fromfile(path, dtype='uint64', count=1, offset=40)
-time = np.fromfile(path, dtype='float64', count=1, offset=48)
-dtype = np.fromfile(path, dtype='uint64', count=1, offset=56)
+imax = np.fromfile(file, dtype='uint64', count=1, offset=0)
+jmax = np.fromfile(file, dtype='uint64', count=1, offset=8)
+kmax = np.fromfile(file, dtype='uint64', count=1, offset=16)
+nvar = np.fromfile(file, dtype='uint64', count=1, offset=24)
+gc   = np.fromfile(file, dtype='uint64', count=1, offset=32)
+step = np.fromfile(file, dtype='uint64', count=1, offset=40)
+time = np.fromfile(file, dtype='float64', count=1, offset=48)
+dtype = np.fromfile(file, dtype='uint64', count=1, offset=56)
 
-print("%s info:"%(path))
+file.close()
+
+print("%s info:"%(sys.argv[1]))
 print("size=(%d %d %d %d)"%(imax, jmax, kmax, nvar))
 print("gc=%d"%(gc))
 print("step=%d"%(step))
