861fd726e46c752ca3d252dea8b525491f989cfe
ki2098 <1336670149@qq.com>
2024-05-08 12:29:56 +0900

    add data file info output

--- script/file-info.py
+++ script/file-info.py
@@ -3,14 +3,14 @@ import sys
 
 path = sys.argv[1]
 
-imax = np.fromfile(path, dtype='uint64', count=1)
-jmax = np.fromfile(path, dtype='uint64', count=1)
-kmax = np.fromfile(path, dtype='uint64', count=1)
-nvar = np.fromfile(path, dtype='uint64', count=1)
-gc   = np.fromfile(path, dtype='uint64', count=1)
-step = np.fromfile(path, dtype='uint64', count=1)
-time = np.fromfile(path, dtype='float64', count=1)
-dtype = np.fromfile(path, dtype='uint64', count=1)
+imax = np.fromfile(path, dtype='uint64', count=1, offset=0)
+jmax = np.fromfile(path, dtype='uint64', count=1, offset=8)
+kmax = np.fromfile(path, dtype='uint64', count=1, offset=16)
+nvar = np.fromfile(path, dtype='uint64', count=1, offset=24)
+gc   = np.fromfile(path, dtype='uint64', count=1, offset=32)
+step = np.fromfile(path, dtype='uint64', count=1, offset=40)
+time = np.fromfile(path, dtype='float64', count=1, offset=48)
+dtype = np.fromfile(path, dtype='uint64', count=1, offset=56)
 
 print("%s info:"%(path))
 print("size=(%d %d %d %d)"%(imax, jmax, kmax, nvar))
