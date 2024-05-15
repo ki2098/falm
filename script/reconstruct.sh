a55367bf74dcb34a8da4cb2fc53b8fcd8c6021de
ki2098 <1336670149@qq.com>
2024-05-15 11:23:36 +0900

    add auto reconstructor script

--- script/reconstruct.sh
+++ script/reconstruct.sh
@@ -6,8 +6,10 @@ spawn pjsub --interact -g gg18 -L rscgrp=prepost,node=1
 #適当なコマンドを実行してみる
 expect "$ " {send "pwd\r"} 
 
+expect "$ " {send "cd \work\gg18\g18000\falm\app\nedo\r"} 
+
 #「data\uvpw1.0」の部分は実際のデータパスに変更
-expect "$ " {send "$FALM_HOME\bin\reconstructor $1\r"}
+expect "$ " {send "\work\gg18\g18000\falm\bin\reconstructor $1\r"}
 
 #終了
 expect "$ " {send "exit\r"}
