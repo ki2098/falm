59cabb9f128d1f8821faec8c973069a2feb0cb33
ki2098 <1336670149@qq.com>
2024-05-15 11:31:32 +0900

    add auto reconstructor script

--- script/reconstruct.sh
+++ script/reconstruct.sh
@@ -9,7 +9,7 @@ expect "$ " {send "pwd\r"}
 expect "$ " {send "cd /work/gg18/g18000/falm/app/nedo\r"} 
 
 #「data\uvpw1.0」の部分は実際のデータパスに変更
-expect "$ " {send "/work/gg18/g18000/falm/bin/reconstructor \{\$1\}\r"}
+expect "$ " {send "/work/gg18/g18000/falm/bin/reconstructor \$1\r"}
 
 #終了
 expect "$ " {send "exit\r"}
