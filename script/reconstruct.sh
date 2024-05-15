737e31d5918705dcfbeb4cef67415b33d8f162be
ki2098 <1336670149@qq.com>
2024-05-15 12:26:27 +0900

    add auto reconstructor script

--- script/reconstruct.sh
+++ script/reconstruct.sh
@@ -2,6 +2,7 @@
 # reconstruct.sh [prefix] で実行
 set prefix [lindex $argv 0]
 
+# reconstructorは少し時間がかかるので、タイムアウトを無効化しないと、reconstructor後のコマンドは実行されなくなる
 set timeout -1
 
 # 必ずspawnで実行
@@ -9,7 +10,9 @@ spawn pjsub --interact -g gg18 -L rscgrp=prepost,node=1
 
 # プロンプトを検知したらコマンドを打ち込む
 # 適当なコマンドを実行してみる
-expect "$ " {send "pwd\r"} 
+expect "$ " {send "pwd\r"}
+
+# データは一応/work/gg18/g18000/falm/app/nedo/dataに保存してある
 expect "$ " {send "cd /work/gg18/g18000/falm/app/nedo\r"} 
 
 # reconstructor本体を実行
