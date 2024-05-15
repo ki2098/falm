67da39cf2fc8989ffa9c2725af2f1b5efab607a3
ki2098 <1336670149@qq.com>
2024-05-15 12:18:14 +0900

    add auto reconstructor script

--- script/reconstruct.sh
+++ script/reconstruct.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/expect
-
+# reconstruct.sh [prefix] で実行
 set prefix [lindex $argv 0]
 
-#必ずspawnで実行
+set timeout -1
+
+# 必ずspawnで実行
 spawn pjsub --interact -g gg18 -L rscgrp=prepost,node=1
 
-#適当なコマンドを実行してみる
+# プロンプトを検知したらコマンドを打ち込む
+# 適当なコマンドを実行してみる
 expect "$ " {send "pwd\r"} 
-
 expect "$ " {send "cd /work/gg18/g18000/falm/app/nedo\r"} 
 
-#「data\uvpw1.0」の部分は実際のデータパスに変更
+# reconstructor本体を実行
 expect "$ " {send "/work/gg18/g18000/falm/bin/reconstructor $prefix\r"}
 
-#終了
+# 終了
 expect "$ " {send "exit\r"}
 
-#これを忘れなく
+# これを忘れなく
 interact
