4d8dacf1e78ccfcc7eeb669f0b330e3c09f4dd5e
ki2098 <1336670149@qq.com>
2025-03-21 18:43:03 +0900

    backup

---
+++ app/windtunnel/wisrun.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+#PJM -g gg18
+#PJM -o nedo.log
+#PJM -j
+#PJM -X
+#PJM -L rscgrp=regular-a
+#PJM -L node=1
+#PJM -L elapse=8:00:00
+#PJM --mpi proc=8
+
+. ./env
+
+module load nvidia
+module load nvmpi
+module list
+
+cp wissetup.json setup.json
+
+date
+
+mpiexec -machinefile $PJM_O_NODEINF -n $PJM_MPI_PROC -npernode 8 --report-bindings bin/main
+
+date
